429047793049b1846c1f6fd98270c643a197d6c9
SoloPHP <vedastudio@gmail.com>
2024-12-12 13:40:12 +0200

    add: Auto-Resolution and  Interface Binding

--- README.md
+++ README.md
@@ -1,2 +1,91 @@
-# Container
-PSR-11 compatible Dependency Injection Container
+# Solo PHP Container
+
+A lightweight, PSR-11 compliant dependency injection container for PHP applications.
+
+## Installation
+
+You can install the package via composer:
+
+```bash
+composer require solophp/container
+```
+
+## Requirements
+
+- PHP 8.0 or higher
+- Composer 2.0 or higher
+
+## Basic Usage
+
+```php
+use Solo\Container;
+
+// Create a new container
+$container = new Container();
+
+// Register a service
+$container->set('database', function($container) {
+    return new Database('localhost', 'mydb', 'user', 'pass');
+});
+
+// Bind an interface to a concrete implementation
+$container->bind(LoggerInterface::class, FileLogger::class);
+
+// Get a service
+$db = $container->get('database');
+```
+
+## Features
+
+- PSR-11 Compatible
+- Automatic dependency resolution
+- Interface binding
+- Singleton instances
+- Constructor injection
+- Service factories
+
+## Advanced Usage
+
+### Auto-Resolution
+
+The container can automatically resolve class dependencies:
+
+```php
+class UserRepository
+{
+    public function __construct(
+        private Database $database,
+        private LoggerInterface $logger
+    ) {}
+}
+
+// The container will automatically resolve Database and LoggerInterface
+$userRepo = $container->get(UserRepository::class);
+```
+
+### Multiple Services Registration
+
+```php
+$container = new Container([
+    'config' => fn() => new Config('config.php'),
+    'cache' => fn($c) => new Cache($c->get('config')),
+]);
+```
+
+### Interface Binding
+
+```php
+$container->bind(LoggerInterface::class, FileLogger::class);
+$container->bind(CacheInterface::class, RedisCache::class);
+```
+
+## Error Handling
+
+The container throws two types of exceptions:
+
+- `Solo\Exceptions\NotFoundException`: When a requested service is not found
+- `Solo\Exceptions\ContainerException`: When there's an error resolving a service
+
+## License
+
+The MIT License (MIT). Please see [License File](LICENSE.md) for more information.
--- Solo/Container.php
+++ Solo/Container.php
@@ -6,90 +6,145 @@ use Psr\Container\ContainerInterface;
 use ReflectionClass;
 use ReflectionException;
 use ReflectionNamedType;
-use Exception;
+use Solo\Exceptions\ContainerException;
+use Solo\Exceptions\NotFoundException;
 
+/**
+ * PSR-11 compatible Dependency Injection Container implementation
+ */
 class Container implements ContainerInterface
 {
+    /** @var array<string, callable> */
     private array $services = [];
 
-    public function __construct(array $services = [])
+    /** @var array<string, mixed> */
+    private array $instances = [];
+
+    /** @var array<string, string> */
+    private array $bindings = [];
+
+    /**
+     * Container constructor.
+     *
+     * @param array<string, callable> $initialServices Initial services to register
+     * @throws ContainerException If any of the initial services is not callable
+     */
+    public function __construct(array $initialServices = [])
     {
-        if (!empty($services)) {
-            $this->setMultiple($services);
-        }
+        $this->setMultiple($initialServices);
     }
 
-    public function set(string $id, callable $factory): void
-    {
-        $this->services[$id] = $factory;
-    }
-
+    /**
+     * Register multiple services at once
+     *
+     * @param array<string, callable> $services Array of service factories
+     * @throws ContainerException If any service is not callable
+     */
     public function setMultiple(array $services): void
     {
         foreach ($services as $id => $factory) {
+            if (!is_callable($factory)) {
+                throw new ContainerException("Service [$id] must be a callable.");
+            }
+
             $this->set($id, $factory);
         }
     }
 
-    public function get($id)
+    /**
+     * Register a service factory
+     *
+     * @param string $id Service identifier
+     * @param callable $factory Service factory callable
+     */
+    public function set(string $id, callable $factory): void
     {
-        if ($this->has($id)) {
-            return $this->services[$id]($this);
+        $this->services[$id] = fn($c) => $factory($c);
+    }
+
+    /**
+     * Bind an abstract type to a concrete implementation
+     *
+     * @param string $abstract Abstract type identifier
+     * @param string $concrete Concrete class name
+     */
+    public function bind(string $abstract, string $concrete): void
+    {
+        $this->bindings[$abstract] = $concrete;
+    }
+
+    /**
+     * Check if a service is registered
+     *
+     * @param string $id Service identifier
+     * @return bool Whether the service exists
+     */
+    public function has(string $id): bool
+    {
+        return isset($this->services[$id]) || isset($this->bindings[$id]) || class_exists($id);
+    }
+
+    /**
+     * Get a service from the container
+     *
+     * @param string $id Service identifier
+     * @return mixed Resolved service
+     * @throws NotFoundException If the service is not found
+     * @throws ContainerException If the service cannot be resolved
+     * @throws ReflectionException
+     */
+    public function get(string $id): mixed
+    {
+        if (isset($this->instances[$id])) {
+            return $this->instances[$id];
         }
 
-        try {
-            $reflector = new ReflectionClass($id);
-        } catch (ReflectionException $e) {
-            throw new Exception("Target class [$id] does not exist.", 0, $e);
+        if (isset($this->services[$id])) {
+            $resolved = $this->services[$id]($this);
+        } elseif (isset($this->bindings[$id])) {
+            $resolved = $this->resolve($this->bindings[$id]);
+        } elseif (class_exists($id)) {
+            $resolved = $this->resolve($id);
+        } else {
+            throw new NotFoundException("Service [$id] not found in the container.");
         }
+
+        $this->instances[$id] = $resolved;
+        return $resolved;
+        }
+
+    /**
+     * @throws ContainerException
+     * @throws NotFoundException
+     * @throws ReflectionException
+     */
+    private function resolve(string $id)
+    {
+            $reflector = new ReflectionClass($id);
 
         if (!$reflector->isInstantiable()) {
-            throw new Exception("Target [$id] is not instantiable.");
+            throw new ContainerException("Class [$id] is not instantiable.");
         }
 
         $constructor = $reflector->getConstructor();
-
-        if ($constructor === null) {
+        if (!$constructor) {
             return new $id;
         }
 
-        $parameters = $constructor->getParameters();
-        $dependencies = [];
+        $dependencies = array_map(function ($param) {
+            $type = $param->getType();
 
-        foreach ($parameters as $parameter) {
-            $type = $parameter->getType();
-
-            if (!$type instanceof ReflectionNamedType || $type->isBuiltin()) {
-                if ($parameter->isDefaultValueAvailable()) {
-                    $dependencies[] = $parameter->getDefaultValue();
-                } elseif ($parameter->isVariadic()) {
-                    $dependencies[] = [];
-                } else {
-                    throw new Exception("Unresolvable dependency [$parameter] in class {$parameter->getDeclaringClass()->getName()}");
-                }
+            if ($type instanceof ReflectionNamedType && !$type->isBuiltin()) {
+                return $this->get($type->getName());
             }
 
-            $name = $type->getName();
-
-            try {
-                $dependency = $this->get($name);
-                $dependencies[] = $dependency;
-            } catch (Exception $e) {
-                if ($parameter->isOptional()) {
-                    $dependencies[] = $parameter->getDefaultValue();
-                } else {
-                    $dependency = $this->get($parameter->getType()->getName());
-                    $this->set($name, $dependency);
-                    $dependencies[] = $dependency;
-                }
+            if ($param->isDefaultValueAvailable()) {
+                return $param->getDefaultValue();
             }
-        }
+
+            throw new ContainerException("Cannot resolve dependency [{$param->getName()}].");
+        }, $constructor->getParameters());
 
         return $reflector->newInstanceArgs($dependencies);
-    }
-
-    public function has($id): bool
-    {
-        return isset($this->services[$id]);
     }
 }
